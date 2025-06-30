4964cb5a72aaa5d3256f87ceed83b5c4fc6651eb
osrott61-gh <osrott61@dev.extremerom.org>
2025-06-30 19:02:24 +0200

    sm8550: remove qchdcpkprov
    Signed-off-by: osrott61-gh <osrott61@dev.extremerom.org>

--- platform/sm8550/patches/stock_blobs/customize.sh
+++ platform/sm8550/patches/stock_blobs/customize.sh
@@ -17,3 +17,8 @@ echo "Replacing SoundBooster"
 DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver2060.so"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1100.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+echo "Remove qchdcpkprov"
+DELETE_FROM_WORK_DIR "system" "system/bin/qchdcpkprov"
+DELETE_FROM_WORK_DIR "system" "system/bin/dhkprov"
+DELETE_FROM_WORK_DIR "system" "system/etc/init/dhkprov.rc"

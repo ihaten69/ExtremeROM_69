933e9fe3851d3b68c54c8c4f26777be48fe44316
osrott61-gh <osrott61@dev.extremerom.org>
2025-07-18 21:43:30 +0200

    target: dm1q: patches: camera: add another lib
    Signed-off-by: osrott61-gh <osrott61@dev.extremerom.org>

--- target/dm1q/patches/camera/customize.sh
+++ target/dm1q/patches/camera/customize.sh
@@ -4,6 +4,7 @@ ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsuperresolutionraw_
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsuperresolution_raw.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libdualcam_refocus_image.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libdualcam_portraitlighting_gallery_360.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libDLInterface_aidl.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
 
 # Fix Portrait

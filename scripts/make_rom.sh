94ba2d87e3c89fe2797b65af93c22cf935d13032
ihaten69 <youknowmexd69@gmail.com>
2025-09-03 14:44:20 +0530

    Update make_rom.sh

--- scripts/make_rom.sh
+++ scripts/make_rom.sh
@@ -93,6 +93,8 @@ if $BUILD_ROM; then
     fi
     echo -e "- Deleting source super.img..."
     rm -rf $FW_DIR/SM-S938B_EUX/super.img
+    rm -rf $FW_DIR/SM-S938B_EUX/system_a.img
+    rm -rf $FW_DIR/SM-S938B_EUX/product_a.img
     
     echo -e "Deleting target super.img..."
     rm -rf $FW_DIR/SM-G990B_EUX/super.img
